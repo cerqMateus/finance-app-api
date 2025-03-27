31779556339b0cb7785be54de49cfc02ae1ccc58
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-03-27 10:11:55 -0300

    chore: add postgres and dotenv setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const name = 'a';
+import express from 'express';
+import 'dotenv/config.js';
+
+import { PostgresHelper } from './src/db/postgres/helper.js';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  const results = await PostgresHelper.query('SELECT * FROM users;');
+
+  res.send(JSON.stringify(results));
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+
+export default app;
