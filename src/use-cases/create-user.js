e8de6d9aba4476f7f98f44e5d0ae4295d5c8d823
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-03-27 16:16:29 -0300

    feat:add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js';
+
+export class createUserUseCase {
+  async execute(createUserParams) {
+    //TODO: verificar se email já está em uso
+
+    //gerar ID do usuario
+    const userId = uuidv4();
+
+    //criptografar a senha
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    //inserir o usuario no bando de dados
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    //chamar o repositorio
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+    const createdUser = await postgresCreateUserRepository.execute(user);
+
+    return createdUser;
+  }
+}
