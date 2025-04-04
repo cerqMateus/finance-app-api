79b9e3abfeae931e1bd54ac72e88283783cb7c2f
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-04-04 08:22:51 -0300

    fix: transaction is returned on post

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -3,7 +3,7 @@ import { PostgresHelper } from '../../../db/postgres/helper.js';
 export class PostgresCreateTransactionRepositoy {
   async execute(createTransactionParams) {
     await PostgresHelper.query(
-      ' INSERT INTO transactions (id, user_id, name, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      ' INSERT INTO transactions (id, user_id, name, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6)',
       [
         createTransactionParams.id,
         createTransactionParams.user_id,
@@ -13,5 +13,12 @@ export class PostgresCreateTransactionRepositoy {
         createTransactionParams.type,
       ],
     );
+
+    const createdTransaction = await PostgresHelper.query(
+      'SELECT * FROM transactions WHERE id = $1;',
+      [createTransactionParams.id],
+    );
+
+    return createdTransaction[0];
   }
 }
