7914148fac0497427ead0027a8c53f2a8ee23ccd
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-04-04 10:04:40 -0300

    feat: add GetTransactionByUserIdController

---
+++ src/controlers/transaction/get-transaction-by-user-id.js
@@ -0,0 +1,46 @@
+import { UserNotFoundError } from '../../errors/user.js';
+import { serverError } from '../helpers.js';
+import { ok } from '../helpers/http.js';
+import { userNotFound } from '../helpers/user.js';
+import {
+  checkIfIdIsValid,
+  invalidIdResponse,
+  requiredFieldIsMissingResponse,
+} from '../helpers/validation.js';
+
+export class GetTransactionByUserIdController {
+  constructor(getTransactionByIdUseCase) {
+    this.getTransactionByIdUseCase = getTransactionByIdUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      //verificar se o userId foi passado como parâmetro
+      const userId = httpRequest.query.userId;
+      if (!userId) {
+        return requiredFieldIsMissingResponse('userId');
+      }
+
+      //verificar se o userId é válido
+      const userIdIsValid = checkIfIdIsValid(userId);
+      if (!userIdIsValid) {
+        return invalidIdResponse();
+      }
+
+      //chamar useCase
+      const transactions = await this.getTransactionByIdUseCase.execute({
+        userId,
+      });
+
+      //retornar resposta http
+      return ok(transactions);
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof UserNotFoundError) {
+        userNotFound();
+      }
+      return serverError;
+    }
+  }
+}
