505ecb288491777398d7fe4e840e889a0bcdc5d7
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-04-11 11:45:31 -0300

    feat: ensure GetUserBalanceRepository use prisma

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,25 +1,47 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresGetUserBalanceRepository {
   async execute(userId) {
-    const balance = await PostgresHelper.query(
-      `
-SELECT
-	SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
-	SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
-	SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
-	(
-        SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) -
-        SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) -
-        SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
-	) AS balance
-FROM transactions
-WHERE user_id = $1;`,
-      [userId],
-    );
+    const totalExpenses = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'EXPENSE',
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    const totalEarnings = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'EARNING',
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    const totalInvestments = await prisma.transaction.aggregate({
+      where: {
+        user_id: userId,
+        type: 'INVESTMENT',
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+
+    const _totalEarnings = totalEarnings._sum.amount || 0;
+    const _totalExpenses = totalExpenses._sum.amount || 0;
+    const _totalInvestments = totalInvestments._sum.amount || 0;
+
+    const balance = _totalEarnings - _totalExpenses - _totalInvestments;
     return {
-      userId,
-      ...balance[0],
+      totalEarnings: _totalEarnings,
+      totalExpenses: _totalExpenses,
+      totalInvestments: _totalInvestments,
+      balance,
     };
   }
 }
