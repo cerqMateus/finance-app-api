9ca0c9175f4f14ddd1bd14c22e30d94ece5ee9ea
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-04-14 14:39:59 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -94,6 +94,28 @@ describe('Create User Controller', () => {
     expect(result.statusCode).toBe(400);
   });
 
+  it('should return 400 if email is not valid', async () => {
+    //arange
+    const createUserUseCase = new CreateUserUseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+    const httpRequest = {
+      body: {
+        first_name: 'Mateus',
+        last_name: 'Cerqueira',
+        email: 'teste',
+        password: 'teste123',
+      },
+    };
+
+    //act
+
+    const result = await createUserController.execute(httpRequest);
+
+    //assert
+
+    expect(result.statusCode).toBe(400);
+  });
+
   it('should return 400 if password is not provided', async () => {
     //arange
     const createUserUseCase = new CreateUserUseStub();
