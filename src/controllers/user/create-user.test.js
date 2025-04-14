b61464366f99d93ac13a7cf013c7d99cb87015fa
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-04-14 15:33:24 -0300

    test: ensure 500 is returned if CreateUserUseCase throws

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -193,4 +193,31 @@ describe('Create User Controller', () => {
     //assert
     expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
   });
+
+  it('should return 500 if CreateUserUseCase throws', async () => {
+    //arrange
+    const createUserUseCase = new CreateUserUseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+    const httpRequest = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+          length: 6,
+        }),
+      },
+    };
+
+    jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    //act
+    const result = await createUserController.execute(httpRequest);
+
+    //assert
+
+    expect(result.statusCode).toBe(500);
+  });
 });
